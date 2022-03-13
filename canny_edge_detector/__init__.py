4fe98a95c0ae345f46107f009afe433a746c5adc
Siddesh Sambasivam Suseela <siddesh.suseela@shopee.com>
2022-03-13 22:02:53 +0800

    refactor package struct

--- canny_edge_detector/__init__.py
+++ canny_edge_detector/__init__.py
@@ -1,2 +1,6 @@
-from .src import *
+from .Image_handler import ImageHandler
+from .sobel_filter import sobel_filter
+from .gaussian_filter import gaussian_filter
+from .canny_filter import canny
+from .ops import convolve
 
--- canny_edge_detector/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Image_handler import ImageHandler
-from .sobel_filter import sobel_filter
-from .gaussian_filter import gaussian_filter
-from .canny_filter import canny
-from .ops import convolve
